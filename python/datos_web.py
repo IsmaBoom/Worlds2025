d49033b6d9d7ddcca3f4439660826330a2bcb748
labradoor13 <adriantoni13@gmail.com>
2025-10-18 18:48:30 +0200

    nuevos partidos

--- python/datos_web.py
+++ python/datos_web.py
@@ -1134,7 +1134,7 @@ def main(links, link):
 if __name__ == "__main__":
     # Inicializar archivos si es necesario
     
-    link = "https://liquipedia.net/leagueoflegends/Match:ID_Wrd25SwR3H_0002"
+    link = "https://liquipedia.net/leagueoflegends/Match:ID_Wrd25SwR3M_0004"
     try:
         with open(os.path.join(BASE_PATH, "links.txt"),"r") as fichero:
             links = json.load(fichero)
